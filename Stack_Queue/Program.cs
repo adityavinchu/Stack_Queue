813626bb9c8b5a7dc6e28ff5e676fcd073ba63d8
aditya <vinchuaditya@gmail.com>
2022-02-14 09:47:24 +0530

    initial

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stk = new Stack();
+
+
+            stk.push(70);
+            stk.push(30);
+            stk.push(56);
+            stk.DisplayAll();
+            stk.Count();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack_Queue/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    public class Stack
+    {
+        public Node head;
+
+        public void push(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+
+        public int Count()
+        {
+            Node temp = head;
+            int count = 0;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+
+        public void DisplayAll()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    Console.Write("->");
+                    temp = temp.next;
+                }
+            }
+            Console.Write("NULL");
+        }
+
+
+    }
+}
